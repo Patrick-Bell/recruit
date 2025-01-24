7c75907402db7c291927262f526a63c43a548390
Patrick Bell <patrick@Patricks-Laptop.local>
2025-01-24 19:51:57 +0000

    update

--- backend/app/controllers/home_controller.rb
+++ backend/app/controllers/home_controller.rb
@@ -1,12 +1,7 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!
 
   def index
-    file_path = Rails.root.join('public', 'index.html')
-    if File.exist?(file_path)
-      render file: file_path, layout: false
-    else
-      render plain: "404 Not Found: index.html not found", status: 404
-    end
+    render file: 'public/index.html', layout: false
   end
 end
