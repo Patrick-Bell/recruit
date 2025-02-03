0230c133f867eb26eec099a562026131535c66b5
Patrick Bell <patrick@Patricks-Laptop.local>
2025-02-03 19:58:54 +0000

    update

--- backend/app/models/applicant.rb
+++ backend/app/models/applicant.rb
@@ -2,19 +2,16 @@ class Applicant < ApplicationRecord
     belongs_to :job
 
     has_one_attached :cv, dependent: :destroy
-    before_destroy :purge_cv  # Ensures the file itself is deleted
 
 
     def cv_url
-        cv.attached? ? Rails.application.routes.url_helpers.rails_blob_url(cv, only_path: true) : ''
-      end
+      cv.attached? ? Rails.application.routes.url_helpers.rails_blob_path(cv, only_path: true) : ''
+    end
+    
 
       private
 
-      def purge_cv
-        cv.purge if cv.attached?  # Deletes the actual file (blob) from storage
-      end
-
+   
    
    
   end
